bbe2147dd43eab32beeaba9fcef75370cc151c9b
DanThePol <daniel@thepolkadots.net>
2021-02-22 18:35:19 +0100

    Stage 1 Preconditions, Station, StationConnectivity

--- src/ch/epfl/tchu/Preconditions.java
+++ src/ch/epfl/tchu/Preconditions.java
@@ -1,4 +1,11 @@
 package ch.epfl.tchu;
 
 public class Preconditions {
+
+    private Preconditions() {}
+
+    public static void checkArgument(boolean shouldBeTrue) {
+        if (!shouldBeTrue) throw new IllegalArgumentException();
+    }
+
 }
