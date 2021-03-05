9e6f4c378d77001b762b1628332858cf8a778e0f
KarlisKV <kvelins@gmail.com>
2021-03-05 15:30:25 +0200

    help

--- src/ch/epfl/tchu/game/Trail.java
+++ src/ch/epfl/tchu/game/Trail.java
@@ -48,7 +48,7 @@ public final class Trail {
         }
 
         List<Trail> possibleTrailsCopy = new ArrayList<>(possibleTrails);
-        Trail longestTrail = new Trail(List.of());
+        Trail longestTrail = new Trail();
 
         do {
 
