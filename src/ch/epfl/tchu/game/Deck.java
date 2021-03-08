48f8ec27bd02ae51ff7737cbccfec8e2666b4096
KarlisKV <kvelins@gmail.com>
2021-03-08 17:18:05 +0200

    Finished Deck class

--- src/ch/epfl/tchu/game/Deck.java
+++ src/ch/epfl/tchu/game/Deck.java
@@ -2,11 +2,15 @@ package ch.epfl.tchu.game;
 
 import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+
+/**
+ * Route class
+ * @author Karlis Velins (325180)
+ * @author Daniel Polka  (326800)
+ */
 
 public final class Deck < C extends Comparable <C >>{
 
@@ -25,14 +29,12 @@ public final class Deck < C extends Comparable <C >>{
 
         Collections.shuffle(shuffledCards, rng);
 
-        Deck <C> deck = new Deck<C>(shuffledCards);
-
-        return deck;
+        return new Deck<>(shuffledCards);
     }
 
     /**
      * Private constructor of Deck class
-     * @param cards (SortedBag<C>)
+     * @param cards (List<C>) list of cards
      */
     private Deck(List<C> cards) {
 
@@ -40,20 +42,36 @@ public final class Deck < C extends Comparable <C >>{
 
     }
 
+    /**
+     * Returns the size of the deck
+     * @return the size of the deck
+     */
     public int size() {
         return cards.size();
     }
 
+    /**
+     * Returns true if the deck is empty
+     * @return true if the deck is empty
+     */
     public boolean isEmpty() {
 
         return cards == null;
     }
 
+    /**
+     * returns a multiset containing the card at the top of the pile
+     * @return a multiset containing the card at the top of the pile
+     */
     public C topCard() {
         Preconditions.checkArgument(!cards.isEmpty());
         return cards.get(0);
     }
 
+    /**
+     * withoutTopCard returns deck without top card
+     * @return Deck<C> without top card
+     */
     public Deck<C> withoutTopCard() {
         Preconditions.checkArgument(!cards.isEmpty());
 
@@ -61,11 +79,14 @@ public final class Deck < C extends Comparable <C >>{
         cardsWithoutTop.remove(0);
 
 
-        Deck <C> deckWithoutTopCard = new Deck<C>(cardsWithoutTop);
-
-        return deckWithoutTopCard;
+        return new Deck<>(cardsWithoutTop);
     }
 
+    /**
+     * topCards returns a multiset containing the top (count) cards from a deck
+     * @param count (int) amount of cards to return
+     * @return count amount of cards from the top of the deck
+     */
     public SortedBag<C> topCards(int count) {
 
         Preconditions.checkArgument(count > 0 && count < size());
@@ -73,7 +94,7 @@ public final class Deck < C extends Comparable <C >>{
 
         List<C> cardsAtTop = cards.subList(0, count);
 
-        SortedBag.Builder<C> cardsToReturn = new SortedBag.Builder<C>();
+        SortedBag.Builder<C> cardsToReturn = new SortedBag.Builder<>();
         for(int i = 0; i < count; i++) {
             cardsToReturn.add(cards.get(i));
         }
@@ -81,6 +102,21 @@ public final class Deck < C extends Comparable <C >>{
 
         return cardsToReturn.build();
 
+    }
+
+    /**
+     * Returns a deck without the top cards
+     * @param count (int) amount of cards to remove from new deck
+     * @return a deck without the top cards
+     */
+
+    public Deck<C> withoutTopCards(int count) {
+
+        Preconditions.checkArgument(count > 0 && count < size());
+
+        List<C> cardsWithoutTop = cards.subList(count, size());
+
+        return new Deck<>(cardsWithoutTop);
 
     }
 
