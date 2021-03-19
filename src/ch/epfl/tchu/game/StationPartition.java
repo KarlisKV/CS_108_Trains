65fb0c40802719877650fa4f28e96571682e1b2c
DanThePol <daniel@thepolkadots.net>
2021-03-19 15:14:12 +0100

    StationPartition works

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+
 public final class StationPartition implements StationConnectivity {
 
     private final Map<Integer, List<Integer>> stationMap;
@@ -17,19 +19,14 @@ public final class StationPartition implements StationConnectivity {
 
     @Override
     public boolean connected(Station s1, Station s2) {
-        if(!stationMap.containsKey(s1.id()) || !stationMap.containsKey(s2.id())) {
 
+        if(representative(s1.id()) != -1) {
+            return stationMap.get(representative(s1.id())).contains(s2.id());
+        } else if(representative(s2.id()) != -1) {
+            return stationMap.get(representative(s2.id())).contains(s1.id());
+        } else {
             return s1.equals(s2);
-
-        } else if(stationMap.containsKey(s1.id())) {
-
-            return stationMap.get(s1.id()).contains(s2.id());
-
-        } else if(stationMap.containsKey(s2.id())) {
-
-            return stationMap.get(s2.id()).contains(s1.id());
-
-        } else return false;
+        }
     }
 
 
@@ -38,9 +35,11 @@ public final class StationPartition implements StationConnectivity {
     public final static class Builder {
 
         private final Map<Integer, List<Integer>> stationMap;
+        private final int stationCount;
 
         public Builder(int stationCount) {
             Preconditions.checkArgument(stationCount >= 0);
+            this.stationCount = stationCount;
             stationMap = new HashMap<>();
             int i = 0;
             do {
@@ -51,26 +50,78 @@ public final class StationPartition implements StationConnectivity {
 
 
         public Builder connect(Station s1, Station s2) {
-            if(stationMap.containsKey(s1.id())) {
+
+            if (stationMap.get(representative(s1.id())).size() > 1 ^ stationMap.get(representative(s2.id())).size() > 1) {
+
+                if(stationMap.get(representative(s1.id())).size() > 1) {
+
+                    stationMap.get(representative(s1.id())).add(s2.id());
+                    stationMap.remove(s2.id());
+
+                } else {
+
+                    stationMap.get(representative(s2.id())).add(s1.id());
+                    stationMap.remove(s1.id());
+                }
+
+            } else if (stationMap.containsKey(s1.id())) {
+
                 stationMap.get(s1.id()).add(s2.id());
                 stationMap.remove(s2.id());
+
             } else if (stationMap.containsKey(s2.id())) {
+
                 stationMap.get(s2.id()).add(s1.id());
                 stationMap.remove(s1.id());
-            } else {
-                //Should never happen
-                throw new IllegalArgumentException();
             }
+
             return this;
         }
+
+
 
         public StationPartition build() {
             return new StationPartition(stationMap);
         }
 
+        private int representative(int stationID) {
+
+            int ID = -1;
+
+            for(int i = 0; i < stationCount; ++i) {
+                if(stationMap.containsKey(i)) {
+                    if(stationMap.get(i).contains(stationID)) {
+                        ID = i;
+                    }
+                }
+            }
+
+            return ID;
+        }
+
 
 
     }
+
+
+    //I wouldn't have copy-pasted code if we were bloody allowed to make bloody public methods lol
+    private int representative(int stationID) {
+
+        int ID = -1;
+
+        int maximumStation = ChMap.stations().size();
+
+        for(int i = 0; i < maximumStation; ++i) {
+            if(stationMap.containsKey(i)) {
+                if(stationMap.get(i).contains(stationID)) {
+                    ID = i;
+                }
+            }
+        }
+
+        return ID;
+    }
+
 
 
 
