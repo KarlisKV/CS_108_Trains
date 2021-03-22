897a5d05dd03ad0f43fef9cbe477a668ff574221
KarlisKV <kvelins@gmail.com>
2021-03-22 13:39:30 +0200

    Week 5 with GameState

--- src/ch/epfl/tchu/game/GameState.java
+++ src/ch/epfl/tchu/game/GameState.java
@@ -1,35 +1,34 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
 
 import java.util.*;
 
 public final class GameState extends PublicGameState {
 
-    private final int ticketsCount;
-    private final PublicCardState cardState;
-    private final PlayerId currentPlayerId;
+
     private final Map<PlayerId, PlayerState> playerState;
-    private final PlayerId lastPlayer;
+    private final Deck<Ticket> tickets;
+    private final CardState privateCardState;
 
     /**
      * Private constructor of GameState class
      * @param ticketsCount
      * @param cardState
-     * @param currentPlayerId
      * @param playerState
      * @param lastPlayer
+     * @param currentPlayerId
      */
     // TODO: 3/21/2021 the playerState is marked for the reason see 3.2.1. i cba to type it
     private GameState(int ticketsCount, PublicCardState cardState, PlayerId currentPlayerId,
-                      Map<PlayerId, PlayerState> playerState, PlayerId lastPlayer) {
+                      Map<PlayerId, PlayerState> playerState, PlayerId lastPlayer, Deck<Ticket> tickets, CardState privateCardState) {
 
         super(ticketsCount, cardState, currentPlayerId, playerState, lastPlayer);
-        this.ticketsCount = ticketsCount;
-        this.cardState = cardState;
-        this.currentPlayerId = currentPlayerId;
+
+        this.privateCardState = privateCardState;
+        this.tickets = tickets;
         this.playerState = playerState;
-        this.lastPlayer = lastPlayer;
     }
 
     /**
@@ -43,6 +42,8 @@ public final class GameState extends PublicGameState {
         SortedBag<Card> allCards = Constants.ALL_CARDS;
         Deck<Card> fullDeck = Deck.of(allCards, rng);
 
+        Deck<Ticket> deckOfTickets = Deck.of(tickets, rng);
+
         List<PlayerId> players = PlayerId.ALL;
         PlayerId startingPlayer = players.get(rng.nextInt());
 
@@ -53,8 +54,9 @@ public final class GameState extends PublicGameState {
         fullDeck.withoutTopCards(Constants.INITIAL_CARDS_COUNT);
 
         PublicCardState initialCardState = new PublicCardState(null, fullDeck.size(), 0);
-        
-        return new GameState(tickets.size(), initialCardState, startingPlayer, initialMap, null);
+        CardState privateState = CardState.of(fullDeck);
+
+        return new GameState(tickets.size(), initialCardState, startingPlayer, initialMap, null, deckOfTickets, privateState);
     }
 
     /**
@@ -73,9 +75,24 @@ public final class GameState extends PublicGameState {
      */
     @Override
     public PlayerState currentPlayerState() {
-        return playerState.get(currentPlayerId);
+        return playerState.get(currentPlayerId());
     }
-    
-    
-    
+
+    public SortedBag<Ticket> topTickets(int count) {
+
+        // TODO: 3/22/2021 dont understand is it the sortedbag of tickets? What does sommet de la pioche means
+        // do i need to create a deck of tickets and then if yes how do I initialize it?
+        Preconditions.checkArgument(count >= 0 && count <= tickets.size());
+
+        return tickets.topCards(count);
+    }
+
+    public GameState withoutTopTickets(int count) {
+        Preconditions.checkArgument(count >= 0 && count <= tickets.size());
+        return new GameState(ticketsCount(), cardState(), currentPlayerId(), playerState, lastPlayer(),tickets.withoutTopCards(count), privateCardState);
+    }
+
+    public Card topCard() {
+        return privateCardState.topDeckCard();
+    }
 }
