dec680d2b453b4cb42f450d880d66354a84dbcbe
DanThePol <daniel@thepolkadots.net>
2021-05-12 10:37:14 +0200

    Updated ObservableGameState

--- src/ch/epfl/tchu/gui/ObservableGameState.java
+++ src/ch/epfl/tchu/gui/ObservableGameState.java
@@ -3,6 +3,7 @@ package ch.epfl.tchu.gui;
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.game.*;
 
+import javax.naming.directory.Attributes;
 import java.util.List;
 
 public class ObservableGameState {
@@ -11,24 +12,42 @@ public class ObservableGameState {
     private PlayerState playerState;
     private PlayerId PID;
 
-    private int myCarCount = playerState.carCount();
-    private int otherDudesCarCount = gameState.playerState(PID.next()).carCount();
+    private int myCarCount = 0;
+    private int otherDudesCarCount = 0;
 
-    private SortedBag<Card> myCards = playerState.cards();
-    private int otherDudesCardCount = gameState.playerState(PID.next()).cardCount();
+    private SortedBag<Card> myCards = null;
+    private int otherDudesCardCount = 0;
 
-    private SortedBag<Ticket> myTickets = playerState.tickets();
-    private int otherDudesTicketCount = gameState.playerState(PID.next()).ticketCount();
+    private SortedBag<Ticket> myTickets = null;
+    private int otherDudesTicketCount = 0;
 
-    private List<Route> myRoutes = playerState.routes();
-    private List<Route> otherDudesRoutes = gameState.playerState(PID.next()).routes();
+    private List<Route> myRoutes = null;
+    private List<Route> otherDudesRoutes = null;
 
 
     public ObservableGameState(PlayerId PID) {
         this.PID = PID;
     }
 
+
+
+
     public void setState(PublicGameState newGameState, PlayerState newPlayerState) {
+
+        gameState = newGameState;
+        playerState = newPlayerState;
+
+        myCarCount = playerState.carCount();
+        otherDudesCarCount = gameState.playerState(PID.next()).carCount();
+
+        myCards = playerState.cards();
+        gameState.playerState(PID.next()).cardCount();
+
+        myTickets = playerState.tickets();
+        otherDudesTicketCount = gameState.playerState(PID.next()).ticketCount();
+
+        myRoutes = playerState.routes();
+        otherDudesRoutes = gameState.playerState(PID.next()).routes();
 
     }
 
